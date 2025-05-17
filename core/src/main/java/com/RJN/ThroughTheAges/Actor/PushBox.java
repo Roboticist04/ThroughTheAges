4c80915214b11311746eb953287c0a5852a1fbec
BRV <roryjshigaki@gmail.com>
2025-05-17 11:42:41 -0400

    I updated the PushBox class which is critical to our story

--- core/src/main/java/com/RJN/ThroughTheAges/Actor/PushBox.java
+++ core/src/main/java/com/RJN/ThroughTheAges/Actor/PushBox.java
@@ -4,10 +4,52 @@ import java.awt.*;
 
 public class PushBox {
 
+    private int X;
+    private int Y;
+    private int speed;
+
+
     public void drawBox(Graphics g) {
 
 
+    }
 
+    public int getX() {
+        return X;
+    }
+    public int getY() {
+        return Y;
+    }
+    public void setX(int aX) {
+        X = aX;
+    }
+    public void setY(int aY) {
+        Y = aY;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    /*public boolean touching() {
+        if () {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }*/
+
+    public void moveLeft()  {
+        setY(getX()+speed);
+    }
+
+    public void moveRight() {
+        setY(getX()-speed);
     }
 
 }
