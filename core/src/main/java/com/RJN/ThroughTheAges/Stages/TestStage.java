2f685599b24612586b555d39a72bcc4e0bcb1398
Joshua <1019526@lcps.org>
2025-05-17 18:52:04 -0400

    Finish removing box2d

--- core/src/main/java/com/RJN/ThroughTheAges/Stages/TestStage.java
+++ core/src/main/java/com/RJN/ThroughTheAges/Stages/TestStage.java
@@ -1,32 +1,21 @@
 package com.RJN.ThroughTheAges.Stages;
 
-import com.RJN.ThroughTheAges.Actor.PushBox;
 import com.RJN.ThroughTheAges.Props.Platform;
-import com.RJN.ThroughTheAges.Props.StaticCollidableProp;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import space.earlygrey.shapedrawer.ShapeDrawer;
-
-import static com.badlogic.gdx.Gdx.graphics;
 
 public class TestStage extends GameStage {
-    private PushBox pushBox;
+    //private PushBox pushBox;
     private Platform platform;
 
     public TestStage(World world){
         super(new FitViewport(1920,1080),world);
         //addActor(new Player());
-        pushBox = new PushBox(world,500,50);
+        //pushBox = new PushBox(world,500,50);
         platform = new Platform( new Texture(Gdx.files.internal("textures/ground.png")),1000,600, 50, 50);
-        addActor(pushBox);
+        //addActor(pushBox);
         addActor(player);
         addActor(platform);
 
