2e64c46f5ea31e9aa4325e4b24bc85317b222eab
BRV <roryjshigaki@gmail.com>
2025-05-17 16:59:46 -0400

    I updated the PushBox class which is critical to our story

--- core/src/main/java/com/RJN/ThroughTheAges/Actor/PushBox.java
+++ core/src/main/java/com/RJN/ThroughTheAges/Actor/PushBox.java
@@ -9,18 +9,8 @@ import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class PushBox extends PhysicsActor {
-    private int speed;
 
     public PushBox(World world, int startX, int startY) {
         super(world, new Sprite(new Texture(Gdx.files.internal("textures/pushbox.png"))), startX, startY);
     }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
 }
-
