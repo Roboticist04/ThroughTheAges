140cd5091b67dbc9d9d7da6e4b4ee0087ceaabff
Joshua <1019526@lcps.org>
2025-05-17 11:02:54 -0400

    The player moves

--- core/src/main/java/com/RJN/ThroughTheAges/TestStage.java
+++ core/src/main/java/com/RJN/ThroughTheAges/TestStage.java
@@ -2,6 +2,7 @@ package com.RJN.ThroughTheAges;
 
 import com.RJN.ThroughTheAges.Actor.Player;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
@@ -15,16 +16,27 @@ import static com.badlogic.gdx.Gdx.graphics;
 
 public class TestStage extends Stage {
     private static final Texture whiteTexture = new Texture(Gdx.files.internal("textures/1WhitePixel.png"));
+    private Player player;
+
     public TestStage(){
-        addActor(new Player());
+        //addActor(new Player());
+        player = new Player();
+        addActor(player);
     }
 
     public void draw(){
+        processInputs();
         getBatch().begin();
         ShapeDrawer shapeDrawer = new ShapeDrawer(getBatch(), new TextureRegion(whiteTexture));
         shapeDrawer.setColor(new Color(0,0,1,1));
         shapeDrawer.filledRectangle(0,0,graphics.getWidth(),graphics.getHeight());
         getBatch().end();
         super.draw();
+    }
+
+    public void processInputs(){
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            player.moveUp();
+        }
     }
 }
