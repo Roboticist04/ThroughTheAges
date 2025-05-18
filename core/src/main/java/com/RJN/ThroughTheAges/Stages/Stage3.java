325388cf532249f4e7262d5c6309c788425e4149
Joshua <1019526@lcps.org>
2025-05-18 09:36:51 -0400

    Beginning of stage 3

--- core/src/main/java/com/RJN/ThroughTheAges/Stages/Stage3.java
+++ core/src/main/java/com/RJN/ThroughTheAges/Stages/Stage3.java
@@ -1,10 +1,16 @@
 package com.RJN.ThroughTheAges.Stages;
 
 import com.RJN.ThroughTheAges.Main;
+import com.RJN.ThroughTheAges.Props.Platform;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Stage3 extends GameStage{
+    private final Texture groundTexture = new Texture(Gdx.files.internal("textures/ground.png"));
 
     public Stage3(Main main) {
         super(main);
+        //ground
+        addActor(new Platform(groundTexture,0,0,getWidth()/5,40));
     }
 }
