2b8a84b74c99a4e5f4494efcc724eb6c559a500d
BRV <roryjshigaki@gmail.com>
2025-05-18 09:41:41 -0400

    I updated the PushBox class which is critical to our story

---
+++ core/src/main/java/com/RJN/ThroughTheAges/Props/movingPlat.java
@@ -0,0 +1,56 @@
+package com.RJN.ThroughTheAges.Actor;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class movingPlat {
+    private Sprite sprite;
+    private Vector2 position;
+    private Vector2 velocity;
+    private Vector2 pointA, pointB;
+    private boolean movingToB;
+
+    public movingPlat(Vector2 pointA, Vector2 pointB, float speed) {
+        this.sprite = new Sprite(new Texture("Textures/Platform.png"));
+        this.position = new Vector2(pointA);
+        this.velocity = new Vector2(speed, 0);
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.movingToB = true;
+        this.sprite.setSize(60, 10); // Adjust size as needed
+    }
+
+    public void update(float deltaTime) {
+        position.add(velocity.x * deltaTime, velocity.y * deltaTime);
+
+        if (position.epsilonEquals(pointB, 1f)) {
+            movingToB = false;
+            velocity.scl(-1); // Reverse direction
+        } else if (position.epsilonEquals(pointA, 1f)) {
+            movingToB = true;
+            velocity.scl(-1); // Reverse direction
+        }
+
+        sprite.setPosition(position.x - sprite.getWidth() / 2, position.y - sprite.getHeight() / 2);
+    }
+
+    public void render(SpriteBatch batch) {
+        sprite.draw(batch);
+    }
+
+    public void handlePlayerCollision(Player player) {
+        Rectangle platformBounds = new Rectangle(position.x - sprite.getWidth() / 2, position.y - sprite.getHeight() / 2, sprite.getWidth(), sprite.getHeight());
+        Rectangle playerBounds = new Rectangle(player.getX(), player.getY(), player.getWidth(), player.getHeight());
+
+        if (platformBounds.overlaps(playerBounds)) {
+            // Determine the direction of collision and adjust player's position accordingly
+            if (playerBounds.y + playerBounds.height <= platformBounds.y + platformBounds.height) {
+                // Player is above the platform
+                player.setPosition(platformBounds.x - playerBounds.width / 2, platformBounds.y + platformBounds.height);
+            }
+        }
+    }
+}
