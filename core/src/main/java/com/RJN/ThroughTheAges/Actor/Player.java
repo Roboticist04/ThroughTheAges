8a8e959b60ea6724d1fe9d0ead2ba23d09bca9ae
Joshua <1019526@lcps.org>
2025-05-17 20:54:38 -0400

    Make movement feel much better

--- core/src/main/java/com/RJN/ThroughTheAges/Actor/Player.java
+++ core/src/main/java/com/RJN/ThroughTheAges/Actor/Player.java
@@ -6,17 +6,21 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Player extends GameActor {
     //private Sprite sprite;
-    private static final float speed = 5f;
-    private static final float jumpStrength = 8;
+    private static final float speed = 2.75f;
+    private static final float jumpStrength = 6f;
     private float yVelocity;
+    private float xVelocity;
+    private float lefVelocity;
+    private float rightVelocity;
     //private Body body;
 
     public Player () {
-        super(new TextureRegion(new Texture(Gdx.files.internal("Textures/Player.png"))),20, 20);
+        super(new TextureRegion(new Texture(Gdx.files.internal("Textures/Player.png"))),20, 20,29.4f,59.7f);
         //super(world,new Sprite(new Texture(Gdx.files.internal("Textures/Player.png"))), 60, 60,0.3f,0.3f);
         //Texture texture = new Texture("textures/Player.png");
         //setBounds(sprite.getX()*sprite.getScaleX(), sprite.getY()*sprite.getScaleY(),sprite.getWidth()*sprite.getScaleX(),sprite.getHeight()*sprite.getScaleY());
         yVelocity = 0;
+        xVelocity = 0;
     }
 
     public void moveUp(){
@@ -30,11 +34,13 @@ public class Player extends GameActor {
     }
 
     public void moveLeft(){
-        setX(getX()-speed);
+        //setX(getX()-speed);
+        lefVelocity += touchingSurface ? speed:(0.25f*speed);
     }
 
     public void moveRight(){
-        setX(getX()+speed);
+        //setX(getX()+speed);
+        rightVelocity += touchingSurface ? speed:(0.25f*speed);
     }
 
     public void act(float deltaTime){
@@ -45,5 +51,10 @@ public class Player extends GameActor {
         else{
             yVelocity = 0;
         }
+        setX(getX()-lefVelocity);
+        setX(getX()+rightVelocity);
+        lefVelocity = Math.max(0, lefVelocity-(touchingSurface ? 1.75f:1.25f));
+        rightVelocity = Math.max(0, rightVelocity-(touchingSurface ? 1.75f:1.25f));
+        //xVelocity = xVelocity+((xVelocity<0 ? (touchingSurface ? 0.2f:0.1f):(touchingSurface ? -0.2f:-0.1f)));
     }
 }
