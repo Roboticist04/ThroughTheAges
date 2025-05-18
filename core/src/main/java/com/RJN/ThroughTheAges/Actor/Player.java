b5cfe27ccf1e68c405092dd334929d522f1117c4
Joshua <1019526@lcps.org>
2025-05-17 20:38:52 -0400

    Make jumping feel much better

--- core/src/main/java/com/RJN/ThroughTheAges/Actor/Player.java
+++ core/src/main/java/com/RJN/ThroughTheAges/Actor/Player.java
@@ -7,23 +7,25 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class Player extends GameActor {
     //private Sprite sprite;
     private static final float speed = 5f;
-    private static final float jumpHeight = 200;
-    private boolean canJump;
+    private static final float jumpStrength = 8;
+    private float yVelocity;
     //private Body body;
 
     public Player () {
         super(new TextureRegion(new Texture(Gdx.files.internal("Textures/Player.png"))),20, 20);
-        canJump = false;
         //super(world,new Sprite(new Texture(Gdx.files.internal("Textures/Player.png"))), 60, 60,0.3f,0.3f);
         //Texture texture = new Texture("textures/Player.png");
         //setBounds(sprite.getX()*sprite.getScaleX(), sprite.getY()*sprite.getScaleY(),sprite.getWidth()*sprite.getScaleX(),sprite.getHeight()*sprite.getScaleY());
+        yVelocity = 0;
     }
 
     public void moveUp(){
         //setY(getY()+speed);
         //body.applyLinearImpulse(0f,speed,getX()-(getWidth()/2),getY()-(getHeight()/2),true);
         if(touchingSurface){
-            setY(getY()+jumpHeight);
+            setY(getY()+5);
+            yVelocity = jumpStrength;
+            touchingSurface = false;
         }
     }
 
@@ -37,7 +39,11 @@ public class Player extends GameActor {
 
     public void act(float deltaTime){
         if(!touchingSurface) {
-            setY(getY() - 100 * deltaTime);
+            setY(getY() + yVelocity);
+            yVelocity -= 0.1f;
+        }
+        else{
+            yVelocity = 0;
         }
     }
 }
